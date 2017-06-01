0f475e9b7e9b89aca566e7a6585eb5e5d6755b65
cssummer17 <cssummer17@cssummer17s-Mac-mini.local>
2017-06-01 15:26:53 -0700

    First Commit

---
+++ app/views.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, flash, request, redirect
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from app import app
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('form.html', title="Login Page")
+
+
+logins = []
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	user = request.form['u']
+	mypass = request.form['p']
+	logins.append(user + " " + mypass)
+	print(logins)
+	return redirect('/')
+
+@app.route('/stored', methods=['GET', 'POST'])
+def stored():
+	return render_template('stored.html', logins=logins)
+
---
+++ config.py
@@ -0,0 +1,6 @@
+#Prevent cross site refence attack
+WTF_CRSF_ENABLED = True
+SECRET_KEY = "password"
+
+#Show debug messages (TODO: False on deployment)
+DEBUG = True
