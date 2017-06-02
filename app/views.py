4b8d91f4059566a22332e228ee448ef6e1f2fa3e
cssummer17 <cssummer17@cssummer17s-Mac-mini.local>
2017-06-02 09:38:21 -0700

    Animation HTML added, output from python works

--- app/views.py
+++ app/views.py
@@ -17,9 +17,12 @@ def login():
 	mypass = request.form['p']
 	logins.append(user + " " + mypass)
 	print(logins)
-	return redirect('/')
+	return redirect('/animation')
 
 @app.route('/stored', methods=['GET', 'POST'])
 def stored():
 	return render_template('stored.html', logins=logins)
 
+@app.route('/animation', methods=['GET', 'POST'])
+def animation():
+	return render_template('test.html', logins = logins)
