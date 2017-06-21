77e78ce5dc1c2c8481e670c2676de6488a7589a8
Kanishk Tantia <ktantia@g.hmc.edu>
2017-06-21 11:38:31 -0700

    Added punctuation detection to alliteration engine, now alliteration works properly?

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, flash, request, redirect
 from app import app
 from . import allitdetect
+from string import punctuation
+
+def strip_punctuation(inputString):
+	x = ""
+	for character in inputString:
+		if character in punctuation:
+			break
+		else:
+			x += character
+	print(x)
+	return x
 
 @app.route('/')
 @app.route('/index')
@@ -14,5 +25,4 @@ def login():
 	allitDict = allitdetect.main(inputString)
 	alliteratedWords = [item for sublist in allitDict.values() for item in sublist]
 	print(alliteratedWords)
-	return render_template("animation.html", title="Success", alliteratedWords = alliteratedWords, inputString = inputString)
-
+	return render_template("animation.html", title="Success", alliteratedWords = alliteratedWords, inputString = inputString, strip_punctuation=strip_punctuation)
