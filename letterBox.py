d561e13cf87cbf29a57825789bb23a84f88fe1f6
Celine Park <cpark@g.hmc.edu>
2018-02-13 12:22:43 -0800

    Print angle

--- letterBox.py
+++ letterBox.py
@@ -54,10 +54,12 @@ def getBounding(imagePath, numClusters, resultName):
 
             rect = cv2.minAreaRect(cnt)
             print(rect)
+            print(rect[-1])
+
             h, w = rect[1] # get width and height of rectangle
             box = cv2.boxPoints(rect) # get vertices
             box = np.int0(box) # round to nearest integer
-            print(box)
+            
             rect = box.tolist() # save vertices as a python list
 
             if w not in range(width - 25, width + 10) and h not in range(height - 25, height + 10):
