85ef71b6dfe37bd323a364d9f03e5e58d048c1d3
Celine Park <cpark@g.hmc.edu>
2018-02-28 01:12:51 -0800

    lasdf

--- letterBox.py
+++ letterBox.py
@@ -118,7 +118,7 @@ def getBounding(imagePath, numClusters, resultName):
             print("About to append")
             out = boxAppend("out.png", "crop.png")
             cv2.imwrite("out.png", out)
-    pad("padout.png", out)
+    pad(out, "padout.png")
     outP = cv2.imread("padout.png")
     ocr(outP)
     cv2.imwrite("stuff.png", out)
