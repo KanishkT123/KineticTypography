00a3a5a94104a1c846faa223ec13503f903262ab
Celine Park <cpark@g.hmc.edu>
2018-03-20 18:23:11 -0700

    trying with threshold = 5

--- letterBox.py
+++ letterBox.py
@@ -502,8 +502,15 @@ def frameSubtractBin(imageName1, imageName2):
     #### Mask
     #############################
 
+
+    #Binary threshold, laplace edge detection, display, save
+    ret, thresh = cv2.threshold(image3, 5, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(thresh, 1, 255, cv2.THRESH_BINARY)
+
+
     # Basic threshold
-    ret, thresh = cv2.threshold(image3, 0, 255, cv2.THRESH_BINARY)
+    # Doesn't work...
+    # ret, thresh = cv2.threshold(image3, 0, 255, cv2.THRESH_BINARY)
 
     print("About to write post-Threshold.tif")
     cv2.imwrite("post-Threshold.tif", thresh)
