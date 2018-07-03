e21fbdbc3fb89255ebbcde96e7b0c245b33cf090
cssummer18 <cssummer18@c5.clinic.cs.hmc.edu>
2018-07-03 15:03:35 -0700

    initial commit

---
+++ ReadingKitty/Reading Kitty/Teacher/New Book/NewBookViewController.swift
@@ -0,0 +1,76 @@
+//
+//  NewBookViewController.swift
+//  Reading Kitty
+//
+//  Created by cssummer18 on 7/3/18.
+//  Copyright © 2018 cssummer18. All rights reserved.
+//
+
+import UIKit
+
+class NewBookViewController: UIViewController, UITextFieldDelegate {
+    /********** LOCAL VARIABLES **********/
+    // The text field that the user inputs the book title.
+    @IBOutlet weak var titleBox: UITextField!
+    
+    // Saved book title
+    var bookTitle: String = ""
+    
+    // Error message
+    @IBOutlet weak var error: UITextField!
+    
+    // Reference to levels, books, and devices
+    var modelController = ModelController()
+    
+    
+    /********** VIEW FUNCTIONS **********/
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        titleBox.delegate = self
+        error.textColor = UIColor.clear
+    }
+    
+    // When the user presses return, the text is submitted.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Submit text.
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // When text field is going inactive (through tabbing or returning), it saves the title.
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // Save title.
+        bookTitle = textField.text!
+        
+        return true
+    }
+
+    
+    /********** SEGUE FUNCTIONS **********/
+    // When the user clicks the back button, it sends them to the ... scene.
+    @IBAction func backButton(_ sender: Any) {
+        // Go to the ... scene.
+        //self.performSegue(withIdentifier: "", sender: self)
+    }
+    
+    // When the user clicks the next button, it sends them to the ... scene if a title has been entered. Otherwise, an error message is shown.
+    @IBAction func nextButton(_ sender: Any) {
+        if bookTitle == "" {
+            error.textColor = UIColor.red
+        } else {
+            // Go to the ... scene.
+            //self.performSegue(withIdentifier: "", sender: self)
+        }
+    }
+    
+    // Passing data
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Update the modelController in ...
+        if segue.destination is ViewController {
+            let Destination = segue.destination as? ViewController
+            Destination?.modelController = modelController
+            // pass bookTitle
+        }
+    }
+}
