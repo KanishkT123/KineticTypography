b564443abc1e26df36edf1b732167ab80cfd8f0f
Celine Park <cpark@g.hmc.edu>
2019-03-04 14:16:49 -0800

    added "get" resource?

--- youtubeAPI.py
+++ youtubeAPI.py
@@ -1,6 +1,12 @@
 # Authorize server-to-server interactions from Google Compute Engine.
 import httplib2
+import requests
+import json
 from oauth2client.contrib import gce
 
 credentials = gce.AppAssertionCredentials(scope='https://www.googleapis.com/youtube/v3')
 http = credentials.authorize(httplib2.Http())
+
+resp = requests.get('https://www.googleapis.com/youtube/v3')
+mydict = json.loads(resp.content)
+print(mydict)
